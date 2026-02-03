9e91b87dd8a2659fcc78db0c9a85f4452a1f8ccb
Spchfonn <supitcha.manprakhon@gmail.com>
2026-02-03 07:59:12 +0700

    fix layout

--- app/(app)/[cycleId]/evaluatee/layout.tsx
+++ app/(app)/[cycleId]/evaluatee/layout.tsx
@@ -1,39 +1,5 @@
-"use client";
-import React, { useState } from "react";
-import NotificationDrawer from "@/components/NotificationDrawer";
-import { NotificationProvider, useNotifications } from "@/components/NotificationProvider";
-import UserNavBarForSummaryKpi from "@/components/user/UserNavBarForSummaryKpi";
+import React from "react";
 
-function Shell({ children }: { children: React.ReactNode }) {
-	const [openNoti, setOpenNoti] = useState(false);
-	const { notifications, unreadCount, refetch } = useNotifications();
-
-	return (
-		<>
-		<UserNavBarForSummaryKpi
-			unreadCount={unreadCount}
-			onOpenNoti={() => {
-				setOpenNoti(true);
-				refetch("all");
-			}}
-			onCloseNoti={() => setOpenNoti(false)}
-		/>
-
-		<NotificationDrawer
-			open={openNoti}
-			onClose={() => setOpenNoti(false)}
-			notifications={notifications}
-		/>
-
-		<main>{children}</main>
-		</>
-	);
-}
-
-export default function UserLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<NotificationProvider>
-			<Shell>{children}</Shell>
-		</NotificationProvider>
-	);
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
